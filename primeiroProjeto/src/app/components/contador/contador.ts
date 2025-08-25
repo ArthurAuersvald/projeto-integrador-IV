002662d21f2e78ca0ed53c3dc04ebe0576dbdb69
Arthur Auersvald <117219036+ArthurAuersvald@users.noreply.github.com>
2025-08-25 19:53:32 +0000

    Ferramenta de soma finalizada

---
+++ primeiroProjeto/src/app/components/contador/contador.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contador',
+  standalone: false,
+  templateUrl: './contador.html',
+  styleUrl: './contador.css'
+})
+export class Contador {
+  numeroInicial: number = 0;
+
+  incrementador() {
+    this.numeroInicial++;
+  }
+}
