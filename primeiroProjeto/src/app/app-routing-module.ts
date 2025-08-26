f30c6349d19338e5136a65f373a983adddd4022d
Arthur Auersvald <117219036+ArthurAuersvald@users.noreply.github.com>
2025-08-26 23:26:43 +0000

    Visualização de lista com id, nome e status(true, false)

--- primeiroProjeto/src/app/app-routing-module.ts
+++ primeiroProjeto/src/app/app-routing-module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'estrutura', 
+    loadChildren: () => import('./estrutura/estrutura-routing-module').then(m => m.EstruturaRoutingModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- primeiroProjeto/src/app/estrutura/diretiva/diretiva.ts
+++ primeiroProjeto/src/app/estrutura/diretiva/diretiva.ts
@@ -8,14 +8,25 @@ import { Component } from '@angular/core';
 })
 export class Diretiva {
   mostrarMensagem: boolean = true;
+  fonte: number = 12;
 
   toggleMensagem() {
     this.mostrarMensagem = !this.mostrarMensagem;
   }
 
-  listaItens = [
-    {id: '1', nome: 'Arthur', status: 'Ativo'},
-    {id: '2', nome: 'Jean', status: 'Inativo'},
-    {id: '3', nome: 'Marcos', status: 'Ativo'}
+  aumentarFonte() {
+    this.fonte = this.fonte + 2;
+  }
+
+  diminuirFonte() {
+    this.fonte = this.fonte - 2;
+  }
+
+  alunos = [
+    {id: '1', nome: 'Arthur', status: true},
+    {id: '2', nome: 'Jean', status: true},
+    {id: '3', nome: 'Marcos', status: false
+
+    }
   ];
 }
