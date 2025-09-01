7859614223a8a01291c96d1d5b738a0145065c4f
unknown <arthurauersvald@gmail.com>
2025-09-01 15:18:25 -0300

    aplicada lista com classe dinâmica e exibição condicional

--- catalogoProdutos/src/app/produtos/estoque/estoque.ts
+++ catalogoProdutos/src/app/produtos/estoque/estoque.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './estoque.css'
 })
 export class Estoque {
-
+  produtos = [
+    {id: '1', nome: 'Arroz', estoque: 10},
+    {id: '2', nome: 'Feijao', estoque: 20},
+    {id: '3', nome: 'Macarrao', estoque: 0.}
+  ]
 }
