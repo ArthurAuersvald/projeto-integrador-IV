058a2cf9d107fa56e968b2e23fc71a1aad521f40
unknown <arthurauersvald@gmail.com>
2025-09-01 14:55:03 -0300

    Configuração de rotas

---
+++ catalogoProdutos/src/app/produtos/produtos-module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProdutosRoutingModule } from './produtos-routing-module';
+import { Estoque } from './estoque/estoque';
+
+@NgModule({
+  declarations: [
+    Estoque
+  ],
+  imports: [
+    CommonModule,
+    ProdutosRoutingModule
+  ],
+  exports: [
+    Estoque
+  ]
+})
+export class ProdutosModule { }
